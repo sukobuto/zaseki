7421ea3b98121d75819e5e3cd5bf757ab79fef49
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2018-07-25 20:18:18 +0900

    Heroku 対応

--- zaseki/settings.py
+++ zaseki/settings.py
@@ -32,7 +32,7 @@ if dot_env_path.exists():
 SECRET_KEY = env.str('SECRET_KEY', '--development--')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
